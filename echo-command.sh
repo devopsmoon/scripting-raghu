c62641a2b57acb302b72b759c38f7378675a17c4
devopsmoon <md_syd01@yahoo.co.in>
2021-04-12 16:32:52 +0530

    echo commands

---
+++ echo-command.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "welcome to devops world in chicha house"
+echo -e "welcome to rayan group of companies\n\tlocated in usa"
+echo -e "welcome to ayan group of companies\n\tlocated in united kingdom"
+echo -e "\e[1;31mwelcome to rayan group companies\e[0m"
+echo -e "\e\1;33mwelcome to ayan group of companies\e[0m"
