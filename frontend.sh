d7c4a18354b3bf3bfdf813808969e7e831fc2979
devopsmoon <md_syd01@yahoo.co.in>
2021-04-17 01:49:48 +0530

    case command added line

--- frontend.sh
+++ frontend.sh
@@ -16,4 +16,8 @@ case $1 in
       echo "\e[1;31mInstalling mongodb server\e[0m"
       echo "\e[1;32mFinishing the installation\e[0m"
       ;;
+    *)
+      echo "\e[1;31mInvalidate Input please provide the correct input\[0m"
+      echo "\e[1,32mUsage: $0 Frontend | catalogue | mongodb"
+      ;;
     esac
