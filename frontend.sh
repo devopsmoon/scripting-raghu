53e7c823eb1a9fe06db968684141d1d309a7e930
devopsmoon <md_syd01@yahoo.co.in>
2021-04-17 02:15:09 +0530

    added exit status

--- frontend.sh
+++ frontend.sh
@@ -3,6 +3,16 @@
 # Date:16-04-2021
 # Description: This script will provide the output for
 # Modified Date: 16-04-2021
+USER_ID=$(id -u)
+case $USER_ID in
+    0)
+      ;;
+    *)
+      echo "\e[1;31mYou should be root user to perform\e[0m"
+      exit
+      ;;
+    esac
+
 case $1 in
     frontend)
       echo "\e[1;31mInstalling frontend server\e[0m"
@@ -18,6 +28,6 @@ case $1 in
       ;;
     *)
       echo "\e[1;31mInvalidate Input please provide the correct input\e[0m"
-      echo "\e[1,32mUsage: $0 Frontend | catalogue | mongodb\e[0m"
+      echo "\e[1;32mUsage: $0 Frontend | catalogue | mongodb\e[0m"
       ;;
     esac
